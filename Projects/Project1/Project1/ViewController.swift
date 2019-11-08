443f170d3b9f4058f7d018864bb2a392a35ea49b
Đặng Quang Hưng <hungdq58@gmail.com>
2019-11-09 04:56:47 +0700

    Initialize project 1

---
+++ Projects/Project1/Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Đặng Quang Hưng on 11/9/19.
+//  Copyright © 2019 Đặng Quang Hưng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
