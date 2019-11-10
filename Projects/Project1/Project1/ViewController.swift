f38358781c63e7dc98c8f1814ecd99d50b823f6b
Đặng Quang Hưng <hungdq58@gmail.com>
2019-11-10 19:38:31 +0700

    Day 17: Final tweaks: hidesBarsOnTap and large titles

--- Projects/Project1/Project1/ViewController.swift
+++ Projects/Project1/Project1/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
         let fm = FileManager.default
         if let path = Bundle.main.resourcePath {
             if let items = try? fm.contentsOfDirectory(atPath: path) {
