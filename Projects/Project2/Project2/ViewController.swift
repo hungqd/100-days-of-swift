72e57a8090568f826337314c06ee3d39ba9d9fcf
Đặng Quang Hưng <hungdq58@gmail.com>
2019-11-13 13:58:29 +0700

    Day 20: Guess which flag: random numbers

--- Projects/Project2/Project2/ViewController.swift
+++ Projects/Project2/Project2/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var button3: UIButton!
 
     var countries = [String]()
+    var correctAnswer = 0
     var score = 0
 
     override func viewDidLoad() {
@@ -33,6 +34,9 @@ class ViewController: UIViewController {
     }
 
     func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
