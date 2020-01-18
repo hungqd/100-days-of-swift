b9fcf279230a85482f65791cf970a43ad7dbb486
Đặng Quang Hưng <hungdq58@gmail.com>
2020-01-18 09:19:24 +0700

    Initialize project 4

---
+++ Projects/Project4/Project4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Đặng Quang Hưng on 1/18/20.
+//  Copyright © 2020 Đặng Quang Hưng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
