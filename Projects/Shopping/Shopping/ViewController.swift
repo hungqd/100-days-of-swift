573bbff6dc1bbe38a113080a3321bcc28686a3ef
Đặng Quang Hưng <hungdq58@gmail.com>
2020-05-14 11:45:30 +0700

    Day 32 Initialize project

---
+++ Projects/Shopping/Shopping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopping
+//
+//  Created by Đặng Quang Hưng on 5/14/20.
+//  Copyright © 2020 Đặng Quang Hưng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
