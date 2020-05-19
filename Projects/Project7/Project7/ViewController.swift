6fd424fba2e713560551828ec14701d4a8cc9e53
Đặng Quang Hưng <hungdq58@gmail.com>
2020-05-19 09:48:54 +0700

    Day 33 Initialize project 7

---
+++ Projects/Project7/Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Đặng Quang Hưng on 5/19/20.
+//  Copyright © 2020 Đặng Quang Hưng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
