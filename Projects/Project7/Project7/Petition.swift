0627dcbfa310a2fde93a3f8c2a020bb699387524
Đặng Quang Hưng <hungdq58@gmail.com>
2020-05-19 11:08:51 +0700

    Day 33 Project 7 part 1

---
+++ Projects/Project7/Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Đặng Quang Hưng on 5/19/20.
+//  Copyright © 2020 Đặng Quang Hưng. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
