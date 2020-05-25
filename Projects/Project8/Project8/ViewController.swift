15fd763dd8c580d668b4f40351f67a03fdb699d7
Đặng Quang Hưng <hungdq58@gmail.com>
2020-05-25 09:33:53 +0700

    Day 36 Initialize project 8

---
+++ Projects/Project8/Project8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Đặng Quang Hưng on 5/25/20.
+//  Copyright © 2020 Đặng Quang Hưng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
